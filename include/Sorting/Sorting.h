df3f0130cc049ee11d752c6b1b546b480605e3b2
Radik Zabirov <zabirovradik070@gmail.com>
2023-11-26 17:05:41 +0400

    example3 complete

--- include/Sorting/Sorting.h
+++ include/Sorting/Sorting.h
@@ -26,8 +26,7 @@ stats selection_sort(vector<int>& arr)
         size_t min_index = i;
         for (size_t j = i + 1; j < size; ++j)
         {
-            if (arr[j] < arr[min_index])
-            {
+            if (arr[j] < arr[min_index]){
                 min_index = j;
             }
             ++result.comparison_count;
@@ -50,10 +49,9 @@ stats shell_sort(vector<int>& arr)
     for (step = size / 2; step > 0; step /= 2)
         for (i = step; i < size; ++i)
         {
-            size_t tmp = arr[i];
+            int tmp = arr[i];
             ++result.copy_count;
-            for (j = i; j >= step; j -= step)
-            {
+            for (j = i; j >= step; j -= step){
                 ++result.comparison_count;
                 if (tmp < arr[j - step])
                     arr[j] = arr[j - step];
@@ -64,6 +62,7 @@ stats shell_sort(vector<int>& arr)
         }
     return result;
 }
+
 
 void heapify(vector<int>& arr, size_t size, size_t i, stats& result)
 {
@@ -106,10 +105,116 @@ stats heap_sort(vector<int>& arr)
     return result;
 }
 
-vector<int> generateRandomVector(size_t n) {
+
+
+vector<int> generate_random_vector(size_t n) {
     vector<int> result;
-    for (size_t i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         result.push_back(rand() % 100);
+    }
+    return result;
+}
+
+
+vector<int> generate_sorted_vector(size_t n) {
+    vector<int> result;
+    for (int i = 0; i < n; ++i) {
+        result.push_back(i);
+    }
+    return result;
+}
+
+vector<int> generate_inverted_vector(size_t n) {
+    vector<int> result;
+    for (int i = n - 1; i >= 0; --i) {
+        result.push_back(i);
+    }
+    return result;
+}
+
+
+template<typename Iterator>
+stats selection_sort(Iterator begin, Iterator end) {
+    stats result;
+    for (Iterator i = begin; i != end; ++i) {
+        Iterator min_element = i;
+        for (Iterator j = i + 1; j != end; ++j)
+        {
+            if (*j < *min_element) {
+                min_element = j;
+            }
+            ++result.comparison_count;
+        }
+        if (min_element != i) {
+            std::swap(*i, *min_element);
+            ++result.copy_count;
+        }
+    }
+    return result;
+}
+
+
+template<typename Iterator>
+stats shell_sort(Iterator begin, Iterator end)
+{
+    stats result;
+    Iterator j, i;
+    size_t  size = end - begin;
+    for (size_t step = size / 2; step > 0; step /= 2) {
+        for (i = begin + step; i < end; ++i) {
+            int tmp = *i;
+            ++result.copy_count;
+            for (j = i; j - begin >= step; j -= step) {
+                ++result.comparison_count;
+                if (tmp < *(j - step))
+                    *j = *(j - step);
+                else
+                    break;
+            }
+            *j = tmp;
+        }
+    }
+    return result;
+}
+
+template<typename Iterator>
+void heapify(Iterator begin, Iterator end, size_t i, stats& result)
+{
+    size_t largest = i, size = end - begin;
+    size_t left = 2 * i + 1;
+    size_t right = 2 * i + 2;
+
+    if (left < size && *(begin + left) > *(begin + largest)) {
+        largest = left;
+        ++result.comparison_count;
+    }
+
+    if (right < size && *(begin + right) > *(begin + largest)) {
+        largest = right;
+        ++result.comparison_count;
+    }
+
+    if (largest != i)
+    {
+        swap(*(begin + i), *(begin + largest));
+        ++result.copy_count;
+        heapify(begin, end, largest, result);
+    }
+}
+
+template<typename Iterator>
+stats heap_sort(Iterator begin, Iterator end)
+{
+    stats result;
+    size_t size = end - begin;
+    for (int i = size / 2 - 1; i >= 0; i--)
+        heapify(begin, end, i, result);
+
+    for (int i = size - 1; i >= 0; i--)
+    {
+        swap(*begin, *(begin + i));
+        ++result.copy_count;
+        heapify(begin, begin + i, 0, result);
     }
     return result;
 }
--- tests/Sorting_tests.cc
+++ tests/Sorting_tests.cc
@@ -17,6 +17,7 @@
 //	vector<int> v = { 9,5,6,8,1,0,4,2,3,7 };
 //	vector<int> v_sorted = { 0,1,2,3,4,5,6,7,8,9 };
 //	stats statistic = shell_sort(v);
+//	cout << v.size() << endl;
 //	cout << "Comparison_count:" << statistic.comparison_count << " copy_count:" << statistic.copy_count << endl;
 //	EXPECT_TRUE(v_sorted == v);
 //}
@@ -37,56 +38,160 @@
 //	cout << "Comparison_count:" << statistic.comparison_count << " copy_count:" << statistic.copy_count << endl;
 //}
 
-#define SIZE 50000
 
 
-TEST(Example2, AverageShellSort) {
-	size_t sum_comparison = 0, sum_copy = 0;
-	for (size_t i = 0; i < 100; ++i) {
-		srand(i);
-		vector<int> Tmp = generateRandomVector(SIZE);
-		//vector<int> gmp = Tmp;
-		stats tmp = shell_sort(Tmp);
-		//sort(gmp.begin(), gmp.end());
-		//EXPECT_TRUE(Tmp == gmp);
-		sum_comparison += tmp.comparison_count;
-		sum_copy += tmp.copy_count;
-	}
+//
+//TEST(Example2a, AverageShellSort) {
+//	size_t sum_comparison = 0, sum_copy = 0;
+//	for (size_t i = 0; i < 100; ++i) {
+//		srand(i);
+//		vector<int> Tmp = generate_random_vector(SIZE);
+//		//vector<int> gmp = Tmp;
+//		stats tmp = shell_sort(Tmp);
+//		//sort(gmp.begin(), gmp.end());
+//		//EXPECT_TRUE(Tmp == gmp);
+//		sum_comparison += tmp.comparison_count;
+//		sum_copy += tmp.copy_count;
+//	}
+//	cout << "size: " << SIZE << endl;
+//	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+//	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+//}
+//
+//TEST(Example2a, AverageHeapSort) {
+//	size_t sum_comparison = 0, sum_copy = 0;
+//	for (size_t i = 0; i < 100; ++i) {
+//		srand(i);
+//		vector<int> Tmp = generate_random_vector(SIZE);
+//		stats tmp = heap_sort(Tmp);
+//		//vector<int> gmp = Tmp;
+//		/*sort(gmp.begin(), gmp.end());
+//		EXPECT_TRUE(Tmp == gmp);*/
+//		sum_comparison += tmp.comparison_count;
+//		sum_copy += tmp.copy_count;
+//	}
+//	cout << "size: " << SIZE << endl;
+//	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+//	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+//}
+//
+//TEST(Example2a, AverageSelectionSort) {
+//	size_t sum_comparison = 0, sum_copy = 0;
+//	for (size_t i = 0; i < 100; ++i) {
+//		srand(rand() + i);
+//		vector<int> Tmp = generate_random_vector(SIZE);
+//		//vector<int> gmp = Tmp;
+//		stats tmp = selection_sort(Tmp);
+//		//sort(gmp.begin(), gmp.end());
+//		//EXPECT_TRUE(Tmp == gmp);
+//		sum_comparison += tmp.comparison_count;
+//		sum_copy += tmp.copy_count;
+//	}
+//	cout << "size: " << SIZE << endl;
+//	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+//	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+//}
+
+
+//TEST(Example2b, AverageShellSort) {
+//	size_t sum_comparison = 0, sum_copy = 0;
+//	for (size_t i = 0; i < 100; ++i) {
+//		vector<int> Tmp = generate_sorted_vector(SIZE);
+//		stats tmp = shell_sort(Tmp);
+//		sum_comparison += tmp.comparison_count;
+//		sum_copy += tmp.copy_count;
+//	}
+//	cout << "size: " << SIZE << endl;
+//	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+//	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+//}
+
+
+
+
+
+
+//TEST(Example2b, SortedSelectionSort) {
+//	vector<int> Tmp = generate_sorted_vector(SIZE);
+//	stats tmp = selection_sort(Tmp);
+//	cout << "size: " << SIZE << endl;
+//	cout << "comparison quantity: " << tmp.comparison_count << endl;
+//	cout << "copy quantity: " << tmp.copy_count << endl;
+//}
+//TEST(Example2c, InvertedSelectionSort) {
+//	vector<int> Tmp = generate_inverted_vector(SIZE);
+//	stats tmp = selection_sort(Tmp);
+//	cout << "size: " << SIZE << endl;
+//	cout << "comparison quantity: " << tmp.comparison_count << endl;
+//	cout << "copy quantity: " << tmp.copy_count << endl;
+//}
+
+//TEST(Example2b, SortedShellSort) {
+//	vector<int> Tmp = generate_sorted_vector(SIZE);
+//	stats tmp = shell_sort(Tmp);
+//	cout << "size: " << SIZE << endl;
+//	cout << "comparison quantity: " << tmp.comparison_count << endl;
+//	cout << "copy quantity: " << tmp.copy_count << endl;
+//}
+//TEST(Example2c, InvertedShellSort) {
+//	vector<int> Tmp = generate_inverted_vector(SIZE);
+//	stats tmp = shell_sort(Tmp);
+//	cout << "size: " << SIZE << endl;
+//	cout << "comparison quantity: " << tmp.comparison_count << endl;
+//	cout << "copy quantity: " << tmp.copy_count << endl;
+//}
+
+//TEST(Example2b, SortedHeapSort) {
+//	vector<int> Tmp = generate_sorted_vector(SIZE);
+//	stats tmp = heap_sort(Tmp);
+//	cout << "size: " << SIZE << endl;
+//	cout << "comparison quantity: " << tmp.comparison_count << endl;
+//	cout << "copy quantity: " << tmp.copy_count << endl;
+//}
+//TEST(Example2c, InvertedHeapSort) {
+//	vector<int> Tmp = generate_inverted_vector(SIZE);
+//	stats tmp = heap_sort(Tmp);
+//	cout << "size: " << SIZE << endl;
+//	cout << "comparison quantity: " << tmp.comparison_count << endl;
+//	cout << "copy quantity: " << tmp.copy_count << endl;
+//}
+//TEST(Example2b, AverageSelectionSort) {
+//	size_t sum_comparison = 0, sum_copy = 0;
+//	for (size_t i = 0; i < 100; ++i) {
+//		vector<int> Tmp = generate_sorted_vector(SIZE);
+//		stats tmp = selection_sort(Tmp);
+//		sum_comparison += tmp.comparison_count;
+//		sum_copy += tmp.copy_count;
+//	}
+//	cout << "size: " << SIZE << endl;
+//	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
+//	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+//}
+
+//TEST(Example3, SelectionSort) {
+//	vector<int> v = generate_inverted_vector(SIZE);
+//	vector<int> v_sorted = generate_sorted_vector(SIZE);
+//	stats statistic = selection_sort(v.begin(), v.end());
+//	cout << "Comparison_count:" << statistic.comparison_count << " copy_count:" << statistic.copy_count << endl;
+//	EXPECT_TRUE(v_sorted == v);
+//}
+#define SIZE 1000
+
+//TEST(Example3, InvertedShellSort) {
+//	vector<int> Tmp = generate_inverted_vector(SIZE);
+//	vector<int> v_sorted = generate_sorted_vector(SIZE);
+//	stats tmp = shell_sort(Tmp.begin(), Tmp.end());
+//	cout << "size: " << SIZE << endl;
+//	cout << "comparison quantity: " << tmp.comparison_count << endl;
+//	cout << "copy quantity: " << tmp.copy_count << endl;
+//	EXPECT_TRUE(v_sorted == Tmp);
+//}
+TEST(Example3, InvertedHeapSort) {
+	vector<int> Tmp = generate_inverted_vector(SIZE);
+	vector<int> v_sorted = generate_sorted_vector(SIZE);
+	stats tmp = heap_sort(Tmp.begin(), Tmp.end());
 	cout << "size: " << SIZE << endl;
-	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
-	cout << "Average copy quantity: " << sum_copy / 100 << endl;
-}
-
-TEST(Example2, AverageHeapSort) {
-	size_t sum_comparison = 0, sum_copy = 0;
-	for (size_t i = 0; i < 100; ++i) {
-		srand(i);
-		vector<int> Tmp = generateRandomVector(SIZE);
-		stats tmp = heap_sort(Tmp);
-		//vector<int> gmp = Tmp;
-		/*sort(gmp.begin(), gmp.end());
-		EXPECT_TRUE(Tmp == gmp);*/
-		sum_comparison += tmp.comparison_count;
-		sum_copy += tmp.copy_count;
-	}
-	cout << "size: " << SIZE << endl;
-	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
-	cout << "Average copy quantity: " << sum_copy / 100 << endl;
-}
-
-TEST(Example2, AverageSelectionSort) {
-	size_t sum_comparison = 0, sum_copy = 0;
-	for (size_t i = 0; i < 100; ++i) {
-		srand(rand() + i);
-		vector<int> Tmp = generateRandomVector(SIZE);
-		//vector<int> gmp = Tmp;
-		stats tmp = selection_sort(Tmp);
-		//sort(gmp.begin(), gmp.end());
-		//EXPECT_TRUE(Tmp == gmp);
-		sum_comparison += tmp.comparison_count;
-		sum_copy += tmp.copy_count;
-	}
-	cout << "size: " << SIZE << endl;
-	cout << "Average comparison quantity: " << sum_comparison / 100 << endl;
-	cout << "Average copy quantity: " << sum_copy / 100 << endl;
+	cout << "comparison quantity: " << tmp.comparison_count << endl;
+	cout << "copy quantity: " << tmp.copy_count << endl;
+	EXPECT_TRUE(v_sorted == Tmp);
 }
